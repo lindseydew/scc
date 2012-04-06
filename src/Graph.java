780c349b3d1560d5d20fca27bf68e5d2acf30444
Lindsey Dew <lindsey.dew@guardian.co.uk>
2012-04-06 13:50:37 +0100

    starting on the strong connected component algorithm

---
+++ src/Graph.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: lindseydew
+ * Date: 06/04/2012
+ * Time: 13:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Graph {
+    
+    public static void main (String[] args) {
+       System.out.println("hello world");
+        
+    }
+}
